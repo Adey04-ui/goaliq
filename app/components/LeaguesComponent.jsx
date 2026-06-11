32805c6508894121d8d308008c0059733678d9ae
Kenny Odukoya <kennyodukoya001@gmail.com>
2026-06-11 14:23:13 +0100

    nextauth integrated with google sign in

--- app/components/LeaguesComponent.jsx
+++ app/components/LeaguesComponent.jsx
@@ -1,16 +1,24 @@
 "use client"
 
-import { ChevronRight, Search } from "lucide-react"
+import { CheckCircle, ChevronDown, ChevronRight, Search } from "lucide-react"
 import { useState } from "react"
 import { motion } from "framer-motion"
 import useSWR from "swr"
 import Image from "next/image"
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { signIn } from "next-auth/react"
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 function LeaguesComponent() {
   const [filter, setFilter] = useState("topLeagues")
   const [search, setSearch] = useState("")
+  const [seasons, setSeasons] = useState([
+    '2022',
+    '2023',
+    '2024'
+  ])
+  const [selected, setSelected] = useState(seasons[0])
 
   const filterItems = [
     "favourites",
@@ -36,6 +44,9 @@ function LeaguesComponent() {
     }
   )
 
+  const handleSelect = (season) => {
+    setSelected(season)
+  }
   console.log(leagues)
 
   return (
@@ -64,25 +75,59 @@ function LeaguesComponent() {
             placeholder="Search leagues..."
           />
         </div>
+        <div className="dropdown" style={{width: '21.5%'}}>
+          <DropdownMenu.Root modal={false}>
+              <DropdownMenu.Trigger asChild>
+                <button className="button" style={{display: 'flex', width: '100%', background: 'transparent', border: '.5px solid #5c5c5c', padding: '8px 6px', borderRadius: '6px', justifyContent: 'space-between', placeItems: 'center', textTransform: 'capitalize', cursor: 'pointer'}}>
+                  <div style={{ display: 'flex', gap: '6px', fontSize: '13px' }}>
+                    <span className='status'>season</span>
+                    <span className='menu' style={{ color: `${selected.color}` }}>
+                      {selected}
+                    </span>
+                  </div>
+                  <span className="icpon">
+                    <ChevronDown size={16} />
+                  </span>
+                </button>
+              </DropdownMenu.Trigger>
+
+              <DropdownMenu.Portal>
+                <DropdownMenu.Content
+                  className="dropdown-container"
+                  sideOffset={8}
+                >
+                  {seasons.map((season) => (
+                    <DropdownMenu.Item key={season} onSelect={() => handleSelect(season)} className="dropdown-item" style={{  }}>
+                      {season}
+                      {selected === season && <CheckCircle size={15} />}
+                    </DropdownMenu.Item>
+                  ))}
+                </DropdownMenu.Content>
+              </DropdownMenu.Portal>
+            </DropdownMenu.Root>
+        </div>
       </div>
+      <button onClick={() => signIn("google")}>
+      Continue with Google
+    </button>
 
       <div className="allLeaguesList">
         {leagues.map((league) => (
           <div className="eachList" key={league.league.id}>
             <div className="left">
               <div className="leagueImage">
-                <Image width={30} height={30} alt="league_logo" src={league.league.logo} />
+                <Image width={40} height={40} alt="league_logo" src={league.league.logo} style={{objectPosition: 'center', objectFit: 'contain'}} />
               </div>
               <div className="leagueDetails">
                 <div className="leagueName">
                   {league.league.name}
                 </div>
                 <div className="countryName">
-                  <div className="countryImage">
-                    {league.country.flag && (
-                      <Image width={15} height={15} alt="country_flag" src={league.country.flag} />
-                    )}
-                  </div>
+                  {league.country.flag && (
+                    <div className="countryImage">
+                      <Image width={15} height={15} alt="country_flag" src={league.country.flag} style={{borderRadius: '4px'}} />
+                    </div>
+                  )}
                   {league.country.name}
                 </div>
               </div>
---
+++ app/components/LoginComponent.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function LoginComponent() {
+  return (
+    <div>
+      <div className="loginContainer">
+        <div className="header">
+          <span>
+            Sign In
+          </span>
+        </div>
+        <div className="inputs">
+          <div className="inputContainer">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginComponent
