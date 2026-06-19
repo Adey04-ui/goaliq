1fe3345b91501627024a22d83af4fbbac6fbfc08
Kenny Odukoya <kennyodukoya001@gmail.com>
2026-06-19 10:13:35 +0100

    finished favourites handling

--- app/components/LeaguesList.jsx
+++ app/components/LeaguesList.jsx
@@ -3,6 +3,7 @@ import { ChevronRight } from "lucide-react"
 import { toggleFavourite } from "@/services/favourites"
 import { toast } from "react-toastify"
 import { useFavorites } from "@/context/favoriteContext"
+import { motion } from "framer-motion"
 
 function LeaguesList({ leagues, onSelectLeague }) {
   const { favorites, setFavorites } = useFavorites()
@@ -60,46 +61,76 @@ function LeaguesList({ leagues, onSelectLeague }) {
   const favouriteLeagueIds = new Set(
     favorites?.league?.map((fav) => fav.itemId) || []
   )
+  const containerVariants = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.08, // delay between each card
+      },
+    },
+  }
+
+  const itemVariants = {
+    hidden: {
+      x: -40,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        stiffness: 120,
+        damping: 18,
+      },
+    },
+  }
   return (
-    leagues.map((league) => {
-      const isFavourite = favouriteLeagueIds.has(league.league.id)
-      return (
-        <div className="eachList" key={league.league.id} onClick={() => onSelectLeague(league)}>
-          <div className="left">
-            <div className="leagueImage">
-              <Image width={40} height={40} alt="league_logo" src={league.league.logo} style={{ objectPosition: 'center', objectFit: 'contain' }} />
-            </div>
-            <div className="leagueDetails">
-              <div className="leagueName">
-                {league.league.name}
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="show"
+    >
+      {leagues.map((league) => {
+        const isFavourite = favouriteLeagueIds.has(league.league.id)
+        return (
+          <motion.div variants={itemVariants} className="eachList" key={league.league.id} onClick={() => onSelectLeague(league)}>
+            <div className="left">
+              <div className="leagueImage">
+                <Image width={40} height={40} alt="league_logo" src={league.league.logo} style={{ objectPosition: 'center', objectFit: 'contain' }} />
               </div>
-              <div className="countryName">
-                {league.country.flag && (
-                  <div className="countryImage">
-                    <Image width={15} height={15} alt="country_flag" src={league.country.flag} style={{ borderRadius: '4px' }} />
-                  </div>
-                )}
-                {league.country.name}
+              <div className="leagueDetails">
+                <div className="leagueName">
+                  {league.league.name}
+                </div>
+                <div className="countryName">
+                  {league.country.flag && (
+                    <div className="countryImage">
+                      <Image width={15} height={15} alt="country_flag" src={league.country.flag} style={{ borderRadius: '4px' }} />
+                    </div>
+                  )}
+                  {league.country.name}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="right">
-            <div className="favourite-btn" onClick={(e) => {
-              e.stopPropagation()
-              handleFav(league.league)
-            }}>
-              <svg style={{ strokeWidth: 1, height: 22, width: 22, stroke: '#fff', }} viewBox="0 0 24 24"
-                className={`favourite-svg ${isFavourite ? 'filled' : ''}`}>
-                <polygon points="12 3 15 9 22 9 17 14 19 21 12 17 5 21 7 14 2 9 9 9" />
-              </svg>
+            <div className="right">
+              <div className="favourite-btn" onClick={(e) => {
+                e.stopPropagation()
+                handleFav(league.league)
+              }}>
+                <svg style={{ strokeWidth: 1, height: 22, width: 22, stroke: '#fff', }} viewBox="0 0 24 24"
+                  className={`favourite-svg ${isFavourite ? 'filled' : ''}`}>
+                  <polygon points="12 3 15 9 22 9 17 14 19 21 12 17 5 21 7 14 2 9 9 9" />
+                </svg>
+              </div>
+              <div className="navigation-btn">
+                <ChevronRight size={20} color="#5c5c5c" />
+              </div>
             </div>
-            <div className="navigation-btn">
-              <ChevronRight size={20} color="#5c5c5c" />
-            </div>
-          </div>
-        </div>
-      )
-    })
+          </motion.div>
+        )
+      })}
+    </motion.div>
 
   )
 }
